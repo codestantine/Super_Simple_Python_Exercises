cbfe0c6d2b4c3229d7c9b59f85f888420503c540
Amirmohamad Divani <divaniamirmoh@gmail.com>
2024-01-14 04:09:39 +0330

    add Heads or Tails

---
+++ Heads_or_Tails/README.md
@@ -0,0 +1,5 @@
+# Heads or Tails
+
+Generate a random number (1 or 0), Heads for 1 and Tails for 0.
+
+print Heads if the number is one or Tails if the number is zero.
---
+++ Heads_or_Tails/main.py
@@ -0,0 +1,5 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+num = random.randint(0, 1)
+print("Tails" if num == 0 else "Heads")
